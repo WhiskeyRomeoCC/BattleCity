c23a9d7524c1f006fdd33d8fe94fcfbae9c8fa56
WhiskeyRomeoCC <grach_i02@mail.ru>
2023-10-22 16:41:38 +0300

    Add first triangle and shader class

---
+++ src/Renderer/ShaderProgram.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <string>
+
+namespace Renderer {
+	class ShaderProgram {
+	public:
+		ShaderProgram(const std::string& vertexShader, const std::string& fragmentShader);
+		~ShaderProgram();
+		bool isCompled() const { return m_isCompled; }
+		void use() const;
+
+		ShaderProgram() = delete;
+		ShaderProgram(ShaderProgram&) = delete;
+		ShaderProgram& operator=(const ShaderProgram&) = delete;
+		ShaderProgram& operator=(ShaderProgram&& shaderProgram) noexcept;
+		ShaderProgram(ShaderProgram&& shaderProgram) noexcept;
+
+	private:
+		bool createShader(const std::string& source, const GLenum shaderType, GLuint& shaderID);
+
+		bool m_isCompled = false;
+		GLuint m_ID = 0;
+	};
+}
